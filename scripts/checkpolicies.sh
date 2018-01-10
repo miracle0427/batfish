22a467fc3006f9ed1c3e32d8873ec89a92a3e350
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2018-01-10 15:00:26 -0500

    Updated policy checking script to extract verified policies

--- scripts/checkpolicies.sh
+++ scripts/checkpolicies.sh
@@ -8,14 +8,32 @@ fi
 BASEDIR="`dirname $SCRIPTPATH`/.."
 source $BASEDIR/scripts/common_preamble.sh
 
-echo $TESTRIG_DIR
-
+# Get policies to check
+CHECKFILE="$TESTRIG_DIR/policies/tocheck.txt"
 CMDFILE="/tmp/checkpolicies.batfish"
-rm $CMDFILE
-POLICIESFILE="$TESTRIG_DIR/policies/tocheck.txt"
+rm -f $CMDFILE
 
-cat $POLICIESFILE | while read POLICY; do
+cat $CHECKFILE | while read POLICY; do
     echo "get $POLICY" >> $CMDFILE
 done
 
-allinone $BATFISH_ARGS -runmode batch -cmdfile $CMDFILE | tee "$TESTRIG_DIR/policies/checkpolicies.log"
+# Check policies
+echo "*** CHECKING POLICIES ***"
+LOGFILE="$TESTRIG_DIR/policies/checkpolicies.log"
+
+allinone $BATFISH_ARGS -runmode batch -cmdfile $CMDFILE | tee "$LOGFILE"
+
+# Filter checked policies to only include satisfied policies
+VERIFIEDFILE="$TESTRIG_DIR/policies/verified.txt"
+rm -f $VERIFIEDFILE
+
+echo "*** SATISFIED POLICIES ***"
+COUNT=0
+grep -n "Question" $LOGFILE | cut -d':' -f1 | while read STARTLINE; do
+    COUNT=$((COUNT+1))
+    LEN=`tail -n +$STARTLINE $LOGFILE | grep -n -m 1 "Summary" | cut -d':' -f1`
+    VERIFIED=`tail -n +$STARTLINE $LOGFILE | head -n $LEN | grep -c "Verified"`
+    if [ $VERIFIED == 1 ]; then
+        head -n $COUNT $CHECKFILE | tail -n 1 | tee -a $VERIFIEDFILE
+    fi
+done
