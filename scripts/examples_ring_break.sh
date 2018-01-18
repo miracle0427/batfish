879aa96780df2ff414fc337d480960a1efd96663
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2018-01-17 20:53:23 -0500

    Added policies for rings and another type of broken configs

--- scripts/examples_ring_break.sh
+++ scripts/examples_ring_break.sh
@@ -10,6 +10,7 @@ source $BASEDIR/scripts/common_preamble.sh
 
 ORIG_CONFIGS_DIR=$TESTRIG_DIR/unbroken/configs
 BROKEN_CONFIGS_DIR=$TESTRIG_DIR/rm-network_half/configs
+BROKEN_CONFIGS_DIR=$TESTRIG_DIR/rm-neighbor_one/configs
 rm $BROKEN_CONFIGS_DIR/*
 
 mkdir -p $BROKEN_CONFIGS_DIR
@@ -17,14 +18,17 @@ mkdir -p $BROKEN_CONFIGS_DIR
 DEVICES=`\ls $ORIG_CONFIGS_DIR | shuf`
 NUM_DEVICES=`echo "$DEVICES" | wc -l`
 NUM_BREAK=$((NUM_DEVICES/2))
+NUM_BREAK=1
 NUM_ORIG=$((NUM_DEVICES - NUM_BREAK))
 
 BREAK_DEVICES=`echo "$DEVICES" | head -n $NUM_BREAK`
 ORIG_DEVICES=`echo "$DEVICES" | tail -n $NUM_ORIG`
 
 echo "$BREAK_DEVICES" | while read DEVICE; do
-    echo "Remove network from BGP stanza on $DEVICE"
-    sed -e 's/network 70\.0\.[0-9]*\.0\/24//' $ORIG_CONFIGS_DIR/$DEVICE > $BROKEN_CONFIGS_DIR/$DEVICE
+#    echo "Remove network from BGP stanza on $DEVICE"
+#    sed -e 's/network 70\.0\.[0-9]*\.0\/24//' $ORIG_CONFIGS_DIR/$DEVICE > $BROKEN_CONFIGS_DIR/$DEVICE
+    echo "Remove neighbor from BGP stanza on $DEVICE"
+    sed -e 's/neighbor 10\.0\.[0-9]*\.0.*$//' $ORIG_CONFIGS_DIR/$DEVICE > $BROKEN_CONFIGS_DIR/$DEVICE
 done
 
 echo "$ORIG_DEVICES" | while read DEVICE; do
--- scripts/examples_ring_policies.sh
+++ scripts/examples_ring_policies.sh
@@ -11,8 +11,12 @@ source $BASEDIR/scripts/common_preamble.sh
 CONFIGS_DIR=$TESTRIG_DIR/unbroken/configs
 POLICIES_DIR=$TESTRIG_DIR/unbroken/policies
 mkdir -p $POLICIES_DIR
-rm $POLICIES_DIR/reachability.txt
+rm -f $POLICIES_DIR/reachability.txt $POLICIES_DIR/pathlength.txt
+
+NUM_DEVICES=`\ls $CONFIGS_DIR | wc -l`
+BOUND=$((NUM_DEVICES/2))
 
 \ls $CONFIGS_DIR | while read DEVICE; do
     echo "smt-reachability finalNodeRegex=\"$DEVICE\",finalIfaceRegex=\"Ethernet0\"" >> $POLICIES_DIR/reachability.txt
+    echo "smt-bounded-length finalNodeRegex=\"$DEVICE\",finalIfaceRegex=\"Ethernet0\",bound=$BOUND" >> $POLICIES_DIR/pathlength.txt
 done
