7f814ac6ad8167b4d42c3d01d07b3331a63845b2
anubhavnidhi <anubhavnidhi@cocteau.of.wail.wisc.edu>
2018-01-17 18:51:09 -0600

    checkpointing commit

--- projects/batfish/src/main/java/org/batfish/symbolic/smt/Encoder.java
+++ projects/batfish/src/main/java/org/batfish/symbolic/smt/Encoder.java
@@ -1096,7 +1096,7 @@ public class Encoder {
       List<Symbol> arithlist = _slices.get(MAIN_SLICE_NAME).getAllArithVarsList();
       List<Symbol> bvlist = _slices.get(MAIN_SLICE_NAME).getAllBVVarsList();
       
-      int length = boollist.size() + arithlist.size() + bvlist.size();// + _allFailList.size();
+      int length = boollist.size() + arithlist.size() + bvlist.size() + _allFailList.size();
 
       int index = 0;
       Symbol[] names = new Symbol[length];
@@ -1136,7 +1136,7 @@ public class Encoder {
       _optsolve.Add(king);
       System.out.println(king);
       */
-      //*
+      /*
       length = _allFailList.size();
 
       index = 0;
@@ -1144,20 +1144,20 @@ public class Encoder {
       Sort[] failsorts = new Sort[length];
       //*/
       for (Symbol temp : _allFailList) {
-        failnames[index] = temp;
-        failsorts[index] = getCtx().getIntSort();
+        names[index] = temp;
+        sorts[index] = getCtx().getIntSort();
         index = index + 1;
       }    
-      /*
+      //*
       BoolExpr quick = getCtx().mkForall(sorts,
         names, 
       mkImplies(_modelAnd, _propertRep), 1, null , null, null, null);
-      */
-      BoolExpr quick = getCtx().mkForall(failsorts,
+      //*/
+      /*BoolExpr quick = getCtx().mkForall(failsorts,
         failnames, 
       getCtx().mkExists(sorts, names, mkImplies(_modelAnd, _propertRep),
          1, null , null, null, null), 1, null , null, null, null);
-      
+      */
       /*
       BoolExpr quick = getCtx().mkForall(failsorts,
         failnames, 
