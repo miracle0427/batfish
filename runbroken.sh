b4f9e563d7e009898495fbc47064241be8cf221e
anubhavnidhi <anubhavnidhi@cocteau.of.wail.wisc.edu>
2018-01-19 02:15:08 -0600

    added waypoint, path refs

--- runbroken.sh
+++ runbroken.sh
@@ -1,4 +1,6 @@
-for entry in "subprop"/*
+#source /users/anubhavnidhi/aceso/batfish/tools/batfish_functions.sh
+start=`date +%s`
+for entry in "edgeprops"/*
 do
   echo "$entry"
   cp $entry properties.txt
@@ -6,7 +8,36 @@ do
   entryname=`echo $entry | awk -F"/" '{print $2}'`
   entryname="broke$entryname"
   ./setup.sh $entryname
+  #output="brokeoutput/$entryname.out"
+  #./runz3s.sh $entryname > $output &
+done
+
+
+start=`date +%s`
+for entry in "edgeprops"/*
+do
+  entryname=`echo $entry | awk -F"/" '{print $2}'`
+  entryname="broke$entryname"
   output="brokeoutput/$entryname.out"
+  ./runz3s.sh $entryname > $output &
+done
+echo "Waiting"
+wait
+echo "All edgeprops Z3 done"
+end=`date +%s`
+runtime=$((end-start))
+echo "Total time for running all parallel props in broken config: $runtime" > BrokenOutput
+
+
+for entry in "otherpropnotall"/*
+do
+  echo "$entry"
+  cp $entry properties.txt
+  allinone -cmdfile runbroken.txt
+  entryname=`echo $entry | awk -F"/" '{print $2}'`
+  entryname="broke$entryname"
+  ./setup.sh $entryname
+  output="output/$entryname.out"
   ./runz3s.sh $entryname > $output
   #break
 done
