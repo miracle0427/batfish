93423f9b96a7f86e4b7eb94ca730d63d8786f429
anubhavnidhi <anubhavnidhi@cocteau.of.wail.wisc.edu>
2018-01-20 15:31:21 -0600

    created synthetic config

---
+++ runnetremove.sh
@@ -0,0 +1,41 @@
+#source /users/anubhavnidhi/aceso/batfish/tools/batfish_functions.sh
+start=`date +%s`
+configs="$1/unbroken"
+policies="$configs/policies/path"
+configs="$1/rm-network_half"
+rm -rf pathModel/$policies
+rm -rf pathModelOut/$policies
+mkdir -p pathModel/$policies
+mkdir -p pathModelOut/$policies
+
+for entry in "$policies"/*
+do
+  echo "$entry"
+  cp $entry properties.txt
+  echo "init-testrig $configs" > runfile.txt
+  echo "get smt-mul srcIps=[\"103.0.3.3\"], dstIps=[\"104.0.1.3\"]" >> runfile.txt
+  #cat runfile.txt
+  allinone -cmdfile runfile.txt
+  #entryname=`echo $entry | awk -F"/" '{print $8}'`
+  entryname="pathModel/$entry"
+  ./setup.sh $entryname
+  #echo $entryname
+done
+
+
+start=`date +%s`
+for entry in "$policies"/*
+do
+  #entryname=`echo $entry | awk -F"/" '{print $8}'`
+  entryname="pathModel/$entry"
+  output="pathModelOut/$entry.out"
+  ./runz3s.sh $entryname > $output &
+  #echo $entryname
+  #echo $output
+done
+echo "Waiting"
+wait
+echo "All edgeprops Z3 done"
+end=`date +%s`
+runtime=$((end-start))
+echo "Total time for running all parallel props in rm-neighbor_one $config: $runtime"
---
+++ scripts/examples_fattree_break1.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#if [[ $(uname) == "Darwin" ]]; then
+#    SCRIPTPATH=`greadlink -f $0`
+#else
+#    SCRIPTPATH=`readlink -f $0`
+#fi
+#BASEDIR="`dirname $SCRIPTPATH`/.."
+BASEDIR=/users/anubhavnidhi/aceso/batfish/
+source $BASEDIR/scripts/common_preamble.sh
+TESTRIG_DIR=/users/anubhavnidhi/aceso/batfish/test_rigs/repair-examples/fattree/fattree2
+ORIG_CONFIGS_DIR=$TESTRIG_DIR/unbroken/configs
+BROKEN_CONFIGS_DIR=$TESTRIG_DIR/rm-network_half/configs
+#BROKEN_CONFIGS_DIR=$TESTRIG_DIR/rm-neighbor_one/configs
+rm $BROKEN_CONFIGS_DIR/*
+
+mkdir -p $BROKEN_CONFIGS_DIR
+
+#echo "TEST" $TESTRIG_DIR
+#echo "BASE" $BASEDIR
+
+
+DEVICES=`\ls $ORIG_CONFIGS_DIR | shuf`
+NUM_DEVICES=`echo "$DEVICES" | wc -l`
+#NUM_BREAK=$((NUM_DEVICES/2))
+NUM_BREAK=$((NUM_DEVICES/1))
+#NUM_BREAK=1
+NUM_ORIG=$((NUM_DEVICES - NUM_BREAK))
+
+BREAK_DEVICES=`echo "$DEVICES" | head -n $NUM_BREAK`
+ORIG_DEVICES=`echo "$DEVICES" | tail -n $NUM_ORIG`
+
+EDGE="edge"
+echo "$BREAK_DEVICES" | while read DEVICE; do
+    if [[ $DEVICE =~ $EDGE ]];
+    then
+    	#echo "Remove neighbor from BGP stanza on $DEVICE"
+    	#sed -e 's/neighbor 10\.0\.[0-9]*\.0.*$//' $ORIG_CONFIGS_DIR/$DEVICE > $BROKEN_CONFIGS_DIR/$DEVICE
+	echo "Remove network from BGP stanza on $DEVICE"
+	sed -e 's/network 70\.0\.[0-9]*\.0\/24//' $ORIG_CONFIGS_DIR/$DEVICE > $BROKEN_CONFIGS_DIR/$DEVICE
+    fi
+done
+
+echo "$ORIG_DEVICES" | while read DEVICE; do
+    echo "$DEVICE unmodified"
+    ln -s ../../unbroken/configs/$DEVICE $BROKEN_CONFIGS_DIR/$DEVICE
+done
