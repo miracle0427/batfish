f9372ce2944a60bd862e18e3963b5bc185d3e9a0
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2018-01-20 09:02:22 -0500

    Fixed ring policies to include src/dst IP

--- scripts/examples_ring_policies.sh
+++ scripts/examples_ring_policies.sh
@@ -16,7 +16,15 @@ rm -f $POLICIES_DIR/reachability.txt $POLICIES_DIR/pathlength.txt
 NUM_DEVICES=`\ls $CONFIGS_DIR | wc -l`
 BOUND=$((NUM_DEVICES/2))
 
-\ls $CONFIGS_DIR | while read DEVICE; do
-    echo "smt-reachability finalNodeRegex=\"$DEVICE\",finalIfaceRegex=\"Ethernet0\"" >> $POLICIES_DIR/reachability.txt
-    echo "smt-bounded-length finalNodeRegex=\"$DEVICE\",finalIfaceRegex=\"Ethernet0\",bound=$BOUND" >> $POLICIES_DIR/pathlength.txt
+\ls $CONFIGS_DIR | while read DSTDEV; do
+    DSTNUM=`echo $DSTDEV | sed -e 's/R//'`
+    DSTIP="70.0.$DSTNUM.0/31"
+    \ls $CONFIGS_DIR | while read SRCDEV; do
+        SRCNUM=`echo $SRCDEV | sed -e 's/R//'`
+        SRCIP="70.0.$SRCNUM.0/31"
+        if [ "$SRCIP" != "$DSTIP" ]; then
+            echo "smt-reachability srcIps=['$SRCIP'],dstIps=['$DSTIP'],finalNodeRegex=\"$DSTDEV\",finalIfaceRegex=\"Ethernet0\"" >> $POLICIES_DIR/reachability.txt
+            echo "smt-bounded-length srcIps=['$SRCIP'],dstIps=['$DSTIP'],finalNodeRegex=\"$DSTDEV\",finalIfaceRegex=\"Ethernet0\",bound=$BOUND" >> $POLICIES_DIR/pathlength.txt
+        fi
+    done
 done
