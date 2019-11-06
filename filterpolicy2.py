9947bfd9cc42f31fc2661cb9098b027239aa7f04
anubhavnidhi@cs.wisc.edu <anubhavnidhi@bilbo.of.wail.wisc.edu>
2019-11-06 13:05:26 -0600

    few minor edits

---
+++ filterpolicy2.py
@@ -0,0 +1,45 @@
+import re
+import sys
+
+filename = "verified.txt"
+dstIp = dict()
+preflist = [i for i in range(1,36)]
+preflist.reverse()
+with open(filename, 'r') as infile:
+    for data in infile:
+    #data = infile.read()
+       prop = data.split(" ")
+       #print prop[2]
+       #break
+       args = prop[2].replace("dstIps=","").split("/")[0].replace("\"","").replace("[","").replace("]","").replace(",","")
+       if args not in dstIp:
+          dstIp[args] = list()
+       temp = data
+       for pref in preflist:
+          temp=temp.replace(("/"+str(pref)),"/0")
+       dstIp[args].append(temp)
+
+for ip in dstIp:
+    #print ip
+    dirf = "reach/" + ip
+    outfile = open(dirf, "w+")
+    for line in dstIp[ip]:
+	outfile.write(line)
+'''
+filename = "pathlength.txt"
+dstIp = dict()
+with open(filename, 'r') as infile:
+    for data in infile:
+    #data = infile.read()
+       prop = data.split(" ")
+       args = prop[1].split(",")[1].replace("dstIps=","").split("/")[0].replace("\'","").replace("[","").replace("]","")
+       if args not in dstIp:
+          dstIp[args] = list()
+       dstIp[args].append(data.replace(",",", "))
+
+for ip in dstIp:
+    dirf = "path/" + ip
+    outfile = open(dirf, "w+")
+    for line in dstIp[ip]:
+       outfile.write(line)
+'''
