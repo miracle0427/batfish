0cbde0170cc6d7e82e4a7c4855916457bf2e67d4
sswen <799438891@qq.com>
2022-10-20 20:37:10 +0800

    refactor TpgEdge and TpgNode

--- projects/batfish/src/main/java/org/batfish/multigraph/tpg/TpgEdge.java
+++ projects/batfish/src/main/java/org/batfish/multigraph/tpg/TpgEdge.java
@@ -1,102 +1,50 @@
 package org.batfish.multigraph.tpg;
 
+import lombok.Data;
 import org.batfish.multigraph.graph.EdgeCost;
 import org.batfish.multigraph.graph.protocol;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Collections;
 
+@Data
 public class TpgEdge {
     TpgNode src;
-    TpgNode vertex;
+    TpgNode dst;
     EdgeCost cost;
     protocol type;
     boolean hasACL = false;
     public boolean canRemove = false;
     boolean failed = false;
 
-
-    public static final Map<protocol, Integer> protocol_map = createMap();
-
-    static Map<protocol, Integer> createMap() {
-        Map<protocol, Integer> result = new HashMap<protocol, Integer>();
-        // STAT, OSPF, BGP, REDISOB, REDISBO, IBGP, DEF, NONE;
-        result.put(protocol.STAT, 1);
-        result.put(protocol.OSPF, 100);
-        result.put(protocol.BGP, 20);
-        result.put(protocol.REDISOB, 20);
-        result.put(protocol.REDISBO, 100);
-        result.put(protocol.IBGP, 200);
-        result.put(protocol.DEF, 100);
-        result.put(protocol.NONE, 100000);
-        result.put(protocol.INTF, 10000);
-        result.put(protocol.RIB, 1000);
-        result.put(protocol.REDISSB, 20);
-        result.put(protocol.REDISSO, 100);
-        result.put(protocol.SWITCH, 10000);
-        result.put(protocol.SRC, 0);        
-        result.put(protocol.DST, 0);        
-
-        return Collections.unmodifiableMap(result);
-    }
-
-    public TpgEdge(TpgNode s, TpgNode v, EdgeCost c){
-        src = s; vertex = v; cost = c; type = protocol.NONE;
-        cost.AD = protocol_map.get(type);
-    }
+    public static final Map<protocol, Integer> protocolMap = new HashMap<protocol, Integer>(){
+        {
+            // STAT, OSPF, BGP, REDISOB, REDISBO, IBGP, DEF, NONE;
+            put(protocol.STAT, 1);
+            put(protocol.OSPF, 100);
+            put(protocol.BGP, 20);
+            put(protocol.REDISOB, 20);
+            put(protocol.REDISBO, 100);
+            put(protocol.IBGP, 200);
+            put(protocol.DEF, 100);
+            put(protocol.NONE, 100000);
+            put(protocol.INTF, 10000);
+            put(protocol.RIB, 1000);
+            put(protocol.REDISSB, 20);
+            put(protocol.REDISSO, 100);
+            put(protocol.SWITCH, 10000);
+            put(protocol.SRC, 0);
+            put(protocol.DST, 0);
+        }
+    };
 
     public TpgEdge(TpgNode s, TpgNode v, EdgeCost c, protocol p){
-        src = s; vertex = v; cost = c; type = p;
-        cost.AD = protocol_map.get(type);
+        this(s, v, c, p, false, false);
     }
-
 
     public TpgEdge(TpgNode s, TpgNode v, EdgeCost c, protocol p, boolean acl, boolean removable){
-        src = s; vertex = v; cost = c; type = p; hasACL = acl; canRemove = removable;
-        cost.AD = protocol_map.get(type);
+        src = s; dst = v; cost = c; type = p; hasACL = acl; canRemove = removable;
+        cost.AD = protocolMap.get(type);
     }
-
-    public TpgNode getDst() {
-        return vertex;
-    }
-
-    public TpgNode getSrc() {
-        return src;
-    }
-
-    public EdgeCost getCost() {
-        return cost;
-    }
-
-    public void setCost(EdgeCost ec) {
-        cost = ec;
-    }
-
-    public protocol getType() {
-        return type;
-    }
-
-    public boolean isACL() {
-        return hasACL;
-    }
-
-    public void setACL() {
-        hasACL = true;
-    }
-
-    public void setRemovable() {
-        canRemove = true;
-    }
-
-    @Override
-    public String toString() {
-        return "Edge [src=" + src + ", dst=" + vertex + ", cost=" + cost/* + " type: " + type */ + "]";
-    }
-
-    public TpgEdge copy() {
-        return new TpgEdge(src, vertex, cost.copy(), type, hasACL, canRemove);
-    }
-
 }
 
--- projects/batfish/src/main/java/org/batfish/multigraph/tpg/TpgNode.java
+++ projects/batfish/src/main/java/org/batfish/multigraph/tpg/TpgNode.java
@@ -1,37 +1,11 @@
 package org.batfish.multigraph.tpg;
 
+import lombok.Data;
 import org.batfish.multigraph.graph.protocol;
 
+@Data
 public class TpgNode {
-
 	String id;
     protocol type;
     String device;
-
-    public TpgNode(String x, protocol y, String dev){
-        id = x;
-        type = y;
-        device = dev;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getDevice() {
-        return device;
-    }
-
-    public protocol getProtocol() {
-        return type;
-    }
-
-    public TpgNode copy() {
-        return new TpgNode(id, type, device);
-    }
-
-    @Override
-    public String toString() {
-        return "Node=" + id;// + " Protocol=" + type;
-    }
 }
